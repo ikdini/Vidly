635a18f9c0128ee0fe40d2fef320ea7f8c09a6d1
Dini <ioluwande@gmail.com>
2023-01-17 05:29:23 +0100

    hash

--- hash.js
+++ hash.js
@@ -3,8 +3,10 @@ const bcrypt = require("bcrypt");
 async function run() {
   const salt = await bcrypt.genSalt(10);
   const hashed = await bcrypt.hash("1234", salt);
-  console.log(salt);
-  console.log(hashed);
+  console.log("async salt", salt);
+  console.log("async hashed", hashed);
 }
+
+bcrypt.genSalt(10).then((salt)=> bcrypt.hash("1234", salt)).then((hash)=> console.log(hash)).catch((e)=> console.log(e));
 
 run();
